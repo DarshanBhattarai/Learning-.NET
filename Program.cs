09c5b170a887520e79c5c3177043072171487ebe
DarshanBhattarai <159713572+DarshanBhattarai@users.noreply.github.com>
2025-11-05 09:41:57 +0545

    Workshop 2 initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Week2Workshop
+{
+
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //task 1
+            String userName = "Darshan Bhattarai";
+            int luckyNumber = 7;
+
+            Console.WriteLine($"Hello, {userName}! Your lucky number is {luckyNumber}.");
+
+            //task 2 (1st process)
+            const double PI = 3.14;
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = PI * radius * radius;
+            double perimeter = 2 * PI * radius;
+            Console.WriteLine($"For a circle with radius {radius}:");
+            Console.WriteLine($"Area: {area}");
+            Console.WriteLine($"Perimeter: {perimeter}");
+
+            //task 2 (2nd process)
+            Circle circle = new Circle();
+            //Console.WriteLine($"For a circle with radius {radius}:");
+            Console.WriteLine($"Area: {circle.CalculateArea(3)}");
+            Console.WriteLine($"Perimeter: {circle.CalculatePerimeter(5)}");
+        }
+
+
+        //task 2 (2nd process)
+        public class Circle
+        {
+            public const double PI = 3.14;
+            public double CalculateArea(double radius)
+            {
+                return PI * radius * radius;
+            }
+            public double CalculatePerimeter(double radius)
+            {
+                return 2 * PI * radius;
+            }
+        }
+    }
+}
+
+
