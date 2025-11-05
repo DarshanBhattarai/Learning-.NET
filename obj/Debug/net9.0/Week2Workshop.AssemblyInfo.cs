9ee65b07ea1f4f330d35259e43f4e6818ec5e3ee
DarshanBhattarai <159713572+DarshanBhattarai@users.noreply.github.com>
2025-11-05 22:02:12 +0545

    Completed all workshop tasks: variables, constants, user input, conditional statements, loops, DateTime operations, and generic collections (List & Dictionary).

--- obj/Debug/net9.0/Week2Workshop.AssemblyInfo.cs
+++ obj/Debug/net9.0/Week2Workshop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week2Workshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c5b170a887520e79c5c3177043072171487ebe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week2Workshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week2Workshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
